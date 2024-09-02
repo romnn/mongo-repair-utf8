1aa7f729f9998dd71c7b6d938c35c883ab89b19a
romnn <roman.dahm@gmx.de>
2024-09-02 11:36:18 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,10 @@
-#![allow(warnings)]
-
 use clap::Parser;
 use color_eyre::eyre;
 use dialoguer::Confirm;
+use futures::stream::{self, StreamExt};
 use futures::TryStreamExt;
-use mongodb::{
-    bson::{
-        self, doc,
-        spec::{BinarySubtype, ElementType},
-        Document, RawBsonRef, RawDocument, RawDocumentBuf,
-    },
-    options::{ClientOptions, ServerApi, ServerApiVersion},
-    Client, Cursor,
-};
+use mongodb::{bson, Client};
 use pretty_assertions::Comparison;
-use serde::Deserialize;
-// use serde::Deserialize;
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
@@ -25,21 +14,14 @@ pub struct Options {
     pub connection_uri: String,
     #[arg(long = "database", aliases = ["db"], help = "MongoDB database name")]
     pub database_name: Option<String>,
-    #[arg(long = "collection", aliases = ["col"], help = "MongoDB collection name")]
-    pub collection_name: Option<String>,
+    #[arg(long = "collection", help = "MongoDB collection names")]
+    pub collection_names: Vec<String>,
     #[arg(long = "confirm", help = "Confirm changes interactively")]
     pub confirm: Option<bool>,
 }
 
-// fn fix_document_new<'a>(
-//     doc: bson::RawBsonRef<'a>,
-//     // new_doc: &mut RawDocumentBuf,
-//     confirm: bool,
-// ) -> eyre::Result<bson::RawBson> {
-// }
-
 fn fix_string(
-    doc: &RawDocument,
+    doc: &bson::RawDocument,
     key: &str,
     elem: &bson::raw::RawElement,
     start: usize,
@@ -74,10 +56,10 @@ fn fix_string(
         .ok()
         .map(bson::oid::ObjectId::to_hex);
 
-    let prompt = format!(
-        "[{}][{key}] {old_value_utf8:?} => {new_value_utf8:?}",
-        hex_id.as_deref().unwrap_or(""),
-    );
+    // let prompt = format!(
+    //     "[{}][{key}] {old_value_utf8:?} => {new_value_utf8:?}",
+    //     hex_id.as_deref().unwrap_or(""),
+    // );
     let prompt = format!(
         "[{}][{key}] {}",
         hex_id.as_deref().unwrap_or(""),
@@ -91,18 +73,15 @@ fn fix_string(
 
     Ok(if confirmation {
         println!("{}", &prompt);
-
         new_value_utf8
-        // new_doc.append(key, bson::raw::RawBson::String(new_value_utf8.to_string()));
     } else {
         old_value_utf8
-        // new_doc.append(key, bson::raw::RawBson::String(old_value_utf8.to_string()));
     })
 }
 
 fn fix_document(
-    doc: &RawDocument,
-    new_doc: &mut RawDocumentBuf,
+    doc: &bson::RawDocument,
+    new_doc: &mut bson::RawDocumentBuf,
     confirm: bool,
 ) -> eyre::Result<()> {
     let mut start = 0;
@@ -112,56 +91,38 @@ fn fix_document(
         let value = elem.value();
 
         match elem.element_type() {
-            ElementType::EmbeddedDocument => {
+            bson::spec::ElementType::EmbeddedDocument => {
                 let subdoc = doc.get_document(key)?;
-                // let mut new_subdoc = doc.get_document(key)?.to_raw_document_buf();
-                let mut new_subdoc = RawDocumentBuf::new();
+                let mut new_subdoc = bson::RawDocumentBuf::new();
                 fix_document(subdoc, &mut new_subdoc, confirm)?;
                 new_doc.append(key, new_subdoc);
             }
-            ElementType::Array => {
-                // let array = doc.get_arr(key)?;
-                // let array = doc.get_document(key)?;
+            bson::spec::ElementType::Array => {
                 let array = doc.get_array(key)?;
-                // let array = value?.to_raw_bson();
-                // let array = RawDocument::try_from(value);
-                // let mut new_array = RawDocumentBuf::new();
                 let mut new_array = bson::raw::RawArrayBuf::new();
-                // for elem in value?.iter() {
-                // }
-                // fix_document(array, &mut new_subdoc, confirm)?;
-                // RawDocumentBuf::from(array);
-                // let mut new_array = doc.get_array(key)?.to_raw_array_buf();
-                // for idx in 0..array.len() {
-                // TODO: fix array types
-                // for (idx, item) in array.into_iter().enumerate() {
                 for item in array.into_iter() {
                     match item? {
-                        // bson::raw::RawBsonRef::String() => {
-                        //     new_array.push(bson::raw::RawBson::String(fix_string(
-                        //         doc, key, &elem, start, confirm,
-                        //     )?));
-                        // }
                         bson::raw::RawBsonRef::Document(subdoc) => {
-                            let mut new_subdoc = RawDocumentBuf::new();
+                            let mut new_subdoc = bson::RawDocumentBuf::new();
                             fix_document(subdoc, &mut new_subdoc, confirm)?;
                             new_array.push(new_subdoc);
+                        }
+                        bson::raw::RawBsonRef::String(value) => {
+                            // this is not good enough yet
+                            new_array.push(bson::RawBson::String(
+                                String::from_utf8_lossy(value.as_bytes()).to_string(),
+                            ));
                         }
                         other => {
                             new_array.push(other.to_raw_bson());
                         }
                     }
                 }
-                // }
-                // new_array.push(fix_document_new(item?, confirm)?);
-                //     fix_document(item?.to_raw_bson(), &mut new_subdoc, confirm)?;
-                // }
-
                 new_doc.append(key, new_array);
             }
-            ElementType::String => {
+            bson::spec::ElementType::String => {
                 if let Err(bson::raw::Error {
-                    kind: bson::raw::ErrorKind::Utf8EncodingError(err),
+                    kind: bson::raw::ErrorKind::Utf8EncodingError(_err),
                     ..
                 }) = value
                 {
@@ -173,11 +134,54 @@ fn fix_document(
                     new_doc.append(key, value?.to_raw_bson());
                 }
             }
-            other => {
+            _other => {
                 new_doc.append(key, value?.to_raw_bson());
             }
         }
         start += 1 + key.len() + 1 + elem.len();
+    }
+    Ok(())
+}
+
+async fn fix_collection(
+    collection: mongodb::Collection<bson::RawDocumentBuf>,
+    confirm: bool,
+) -> eyre::Result<()> {
+    let mut cursor = collection.find(bson::doc! {}).await?;
+    while let Some(raw_doc) = cursor.try_next().await? {
+        let mut new_raw_doc = bson::raw::RawDocumentBuf::new();
+
+        println!(
+            "collection = {: <20} id = {: <30}",
+            collection.name(),
+            raw_doc
+                .get_object_id("_id")
+                .ok()
+                .map(bson::oid::ObjectId::to_hex)
+                .as_deref()
+                .unwrap_or("")
+        );
+
+        fix_document(&*raw_doc, &mut new_raw_doc, confirm)?;
+
+        let doc = raw_doc.to_document();
+        let fixed_doc = new_raw_doc.to_document();
+
+        match (doc, fixed_doc) {
+            (Ok(doc), Ok(fixed_doc)) => {
+                print!("{}", Comparison::new(&doc, &fixed_doc));
+                if doc != fixed_doc {
+                    print!("{}", Comparison::new(&doc, &fixed_doc));
+                }
+            }
+            (Err(_doc), Ok(_fixed_doc)) => {
+                // fine
+            }
+            (doc, fixed_doc) => {
+                println!("{:?}", doc);
+                println!("{:?}", fixed_doc);
+            }
+        }
     }
     Ok(())
 }
@@ -194,7 +198,7 @@ async fn main() -> eyre::Result<()> {
     // Send a ping to confirm a successful connection
     client
         .database("admin")
-        .run_command(doc! { "ping": 1 })
+        .run_command(bson::doc! { "ping": 1 })
         .await?;
     println!("connected to {}", options.connection_uri);
 
@@ -203,75 +207,42 @@ async fn main() -> eyre::Result<()> {
         return Ok(());
     };
 
-    let Some(collection_name) = options.collection_name else {
-        println!("no collection specified");
-        return Ok(());
+    let db = client.database(&database_name);
+
+    let collection_names: Vec<String> = if !options.collection_names.is_empty() {
+        options.collection_names
+    } else {
+        db.list_collection_names().await?
+        // let all_collections = db
+        //     .run_command(bson::doc! {
+        //       "listCollections": 1,
+        //       "nameOnly": true,
+        //       "authorizedCollections": true,
+        //     })
+        //     .await?;
+        // println!("{:#?}", all_collections);
+        // all_collections.
+        // Vec::new()
     };
 
-    let db = client.database(&database_name);
-    let collection = db.collection::<RawDocumentBuf>(&collection_name);
+    // let Some(collection_name) = options.collections else {
+    //     println!("no collection specified");
+    //     return Ok(());
+    // };
 
-    let mut cursor = collection.find(doc! {}).await?;
-    while let Some(raw_doc) = cursor.try_next().await? {
-        let mut new_raw_doc = bson::raw::RawDocumentBuf::new();
-        // let mut new_raw_doc = raw_doc.clone();
+    let confirm = options.confirm.unwrap_or(false);
 
-        println!(
-            "{}",
-            raw_doc
-                .get_object_id("_id")
-                .ok()
-                .map(bson::oid::ObjectId::to_hex)
-                .as_deref()
-                .unwrap_or("")
-        );
-
-        // todo: compute diff
-        fix_document(
-            &*raw_doc,
-            &mut new_raw_doc,
-            options.confirm.unwrap_or(false),
-        )?;
-
-        let doc = raw_doc.to_document();
-        let fixed_doc = new_raw_doc.to_document();
-
-        match (doc, fixed_doc) {
-            (Ok(doc), Ok(fixed_doc)) => {
-                if doc != fixed_doc {
-                    print!("{}", Comparison::new(&doc, &fixed_doc));
-                }
+    stream::iter(collection_names.into_iter())
+        .map(|col| {
+            let db_clone = db.clone();
+            async move {
+                let collection = db_clone.collection::<bson::RawDocumentBuf>(&col);
+                fix_collection(collection, confirm).await
             }
-            (Err(doc), Ok(fixed_doc)) => {
-                // fine
-            }
-            (doc, fixed_doc) => {
-                println!("{:?}", doc);
-                println!("{:?}", fixed_doc);
-            }
-        }
+        })
+        .buffered(1)
+        .collect::<Vec<_>>()
+        .await;
 
-        // continue;
-        // for kvp in &raw_doc {
-        //     match &kvp {
-        //         Ok((k, v)) => {
-        //             // println!("k: {:?} v: {:?}", k, v);
-        //         }
-        //         Err(bson::raw::Error {
-        //             kind: bson::raw::ErrorKind::Utf8EncodingError(err),
-        //             ..
-        //         }) => {
-        //             println!("err: {:?}", err);
-        //         }
-        //         Err(err) => {
-        //             println!("other error: {:?}", err);
-        //         }
-        //     }
-        //     // println!("{:?}", kvp); // prints Ok(RawBsonRef) or Err(...)
-        // }
-        // println!("{:?}", doc.des);
-        // println!("{:?}", doc);
-    }
-    // println!("db: {:?}", db);
     Ok(())
 }
